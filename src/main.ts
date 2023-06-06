d44b0c5451f50eb1eb998784fadf6583ef1316d0
nhannv-zigexn <nhannv@zigexn.vn>
2023-06-07 00:43:07 +0700

    Init project and add typescript

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron')
+import path from 'path'
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+// and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, "../index.html"));
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+});
+
